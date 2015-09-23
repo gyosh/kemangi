347942307b3318aea121dc852c77a2ab90beec26
William Gozali <will.gozali@gmail.com>
2015-09-23 20:44:55 +0700

    Polish basic UI

--- src/main/java/com/gyosh/ui/Main.java
+++ src/main/java/com/gyosh/ui/Main.java
@@ -3,21 +3,20 @@ package com.gyosh.ui;
 import javax.swing.*;
 
 public class Main {
-    private JList list1;
-    private JButton button1;
-    private JButton button2;
-    private JButton button3;
-    private JButton vButton;
-    private JButton button7;
-    private JProgressBar progressBar1;
+    private JList taskList;
+    private JButton addTask;
+    private JButton removeTask;
+    private JButton previousTask;
+    private JButton nextTask;
+    private JButton start;
+    private JProgressBar progressBar;
     private JPanel mainPanel;
-    private JTextField textField1;
-    private JButton button5;
-    private JTextField textField2;
-    private JButton button6;
+    private JTextField inputFilePath;
+    private JButton browseInputFile;
+    private JLabel progressLog;
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Main");
+        JFrame frame = new JFrame("Kemangi");
         frame.setContentPane(new Main().mainPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
